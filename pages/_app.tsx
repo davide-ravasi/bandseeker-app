e9aaf14fba1d4ab22ea939aeabd4ae873de7a58b
zxcv3 <zxcv34332@gmail.com>
2021-11-17 06:44:33 +0900

    Delete Global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import { UserProvider } from '@auth0/nextjs-auth0';
+import { UserProvider } from "@auth0/nextjs-auth0";
 
-import GlobalStyle from "../styles/globalStyles";
 import { theme } from "../styles/theme.styles";
 import "./_app.css";
 
